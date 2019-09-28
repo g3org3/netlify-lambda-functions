ff72138041a576216f454889a07797e4deabc0b9
george <7jagjag@gmail.com>
2019-09-28 21:24:19 +0200

    remove top line

--- src/mailgun.js
+++ src/mailgun.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'development') require('dotenv').config()
+// if (process.env.NODE_ENV === 'development') require('dotenv').config()
 
 const { sendMailgunNotification } = require('./util')
 exports.handler = (event, context, callback) => {
